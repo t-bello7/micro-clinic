2d2f22e5fd7a2c70f4cdbe6be984e107d11e619a
brainconnect93 <brainconnect0@gmail.com>
2022-08-31 11:06:00 +0100

    Create the medical history table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE clinic;
 
+-- medical history table 
+CREATE TABLE medical_histories (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INT NOT NULL,
+  status VARCHAR(250) NOT NULL,
+  PRIMARY KEY(id)
+);
